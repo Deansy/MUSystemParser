7cc16cf5a86d6c2d9690a2c3529ab9a40b7a0f68
Cameron Deans <CDeansy@gmail.com>
2015-02-12 19:04:55 +0000

    Created a jUnit test file

---
+++ src/MIUParserTest.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MIUParserTest {
+
+    @org.junit.Test
+    public void testNextStates() throws Exception {
+        MIUParser parser = new MIUParser();
+
+        List<String> miList = new ArrayList<String>();
+        miList.add("MIU");
+        miList.add("MII");
+        //assertEquals(miList, parser.nextStates("MI"));
+
+
+        List<String> miuList = new ArrayList<String>();
+        miuList.add("MIUIU");
+        //assertEquals(miuList, parser.nextStates("MIU"));
+
+
+        List<String> muiList = new ArrayList<String>();
+        muiList.add("MUIU");
+        muiList.add("MUIUI");
+        //assertEquals(muiList, parser.nextStates("MUI"));
+
+
+        List<String> miiiiList = new ArrayList<String>();
+        miiiiList.add("MIIIIU");
+        miiiiList.add("MIIIIIIII");
+        miiiiList.add("MUI");
+        miiiiList.add("MIU");
+       // assertEquals(miiiiList, parser.nextStates("MIIII"));
+
+
+        List<String> muuiiList = new ArrayList<String>();
+        muuiiList.add("MUUIIU");
+        muuiiList.add("MUUIIUUII");
+        muuiiList.add("MII");
+        //assertEquals(muuiiList, parser.nextStates("MUUII"));
+
+        List<String> muuuiList = new ArrayList<String>();
+        muuuiList.add("MUUUIU");
+        muuuiList.add("MUUUIUUUI");
+        muuuiList.add("MUI");
+        //assertEquals(muuuiList, parser.nextStates("MUUUI"));
+
+        List<String> muuiuuList = new ArrayList<String>();
+        muuiuuList.add("MUUIUUUUIUU");
+        muuiuuList.add("MIUU");
+        muuiuuList.add("MUUI");
+        assertEquals(muuiuuList, parser.nextStates("MUUIUU"));
+
+
+    }
+}
